0c677378667fdaf165d65cc0761cf413b05a20fb
Angela Guardia <angelaguardia20@gmail.com>
2021-01-16 14:54:40 -0800

    Update order relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   validates :billing_address, :city, :state, :zip, :cc_name, :cc_number, :status, presence: true
-  
+
   belongs_to :user
+  has_many :order_images
+  has_many :images, through: :order_images
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   describe 'Relationships' do
     it { should belong_to :user }
+    it { should have_many :order_images }
+    it { should have_many(:images).through(:order_images) }
   end
 
   describe 'Validations' do
