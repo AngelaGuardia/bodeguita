307cf275e8c9f2bc243c8299d8aabfef5d314057
Angela Guardia <angelaguardia20@gmail.com>
2021-01-16 12:00:17 -0800

    Add image relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,7 @@
 class User < ApplicationRecord
-  validates_presence_of :first_name,
-                        :last_name,
-                        :address,
-                        :city,
-                        :state,
-                        :zip,
-                        :email
-
+  validates_presence_of :first_name, :last_name, :address, :city, :state, :zip, :email
   validates_uniqueness_of :email
 
+  has_many :images
   enum role: ['buyer', 'photographer']
 end
