acdfda6d77c7c38875be165558695ffd6fe096b1
Angela Guardia <angelaguardia20@gmail.com>
2021-01-16 11:59:07 -0800

    Create images table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_174451) do
+ActiveRecord::Schema.define(version: 2021_01_16_194002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.string "url"
+    t.integer "inventory"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -28,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_01_16_174451) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "images", "users"
 end
