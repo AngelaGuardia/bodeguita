1e9e32c7de62fd9e3b2e8894e18e81f22eb70649
Angela Guardia <angelaguardia20@gmail.com>
2021-01-16 15:01:25 -0800

    Fix rubocop offenses

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
   validates :billing_address, :city, :state, :zip, :cc_name, :cc_number, :status, presence: true
 
   belongs_to :user
-  has_many :order_images
-  has_many :images, through: :order_images
+  has_many :order_images, dependent: :destroy
+  has_many :images, through: :order_images, dependent: :destroy
 end
