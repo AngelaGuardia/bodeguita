e74e3b126bbf00f0eaf413859f3f39939de76389
Angela Guardia <angelaguardia20@gmail.com>
2021-01-16 11:59:38 -0800

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # describe 'Relationships' do
-  # end
+  describe 'Relationships' do
+    it { should have_many :images }
+  end
 
   describe 'Validations' do
-    it {should validate_presence_of :first_name}
-    it {should validate_presence_of :last_name}
-    it {should validate_presence_of :address}
-    it {should validate_presence_of :city}
-    it {should validate_presence_of :state}
-    it {should validate_presence_of :zip}
-    it {should validate_presence_of :email}
-    it {should validate_uniqueness_of :email}
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
   end
 end
