498c61a927d3b6cc7111d5cbbd80646761ceeee2
Angela Guardia <angelaguardia20@gmail.com>
2021-01-16 16:54:02 -0800

    Add createOrder field to mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :update_image, mutation: Mutations::Images::UpdateImage
+    field :create_order, mutation: Mutations::Orders::CreateOrder
   end
 end
